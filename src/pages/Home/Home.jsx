ea80be0a7a1fcb747432768ef07caa6b7bda4ee9
Monwar Hossain <monwarhossainmunna@gmail.com>
2024-01-10 23:04:32 +0600

    [DONE] featured & testimonial page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Featured from "./Featured/Featured";
 import MiddlePart from "./MiddlePart/MiddlePart";
+import Testimonials from "./Testimonials/Testimonials";
 
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
             <MiddlePart></MiddlePart>
             <PopularMenu></PopularMenu>
             <Featured></Featured>
+            <Testimonials />
         </div>
     );
 };
---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,50 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css';
+import icon from '../../../assets/quote.svg';
+const Testimonials = () => {
+    const [reviews, setReview] = useState([]);
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(response => response.json())
+            .then(data => setReview(data))
+    }, [])
+    return (
+        <div className="mb-10">
+            <SectionTitle
+                subHeading={'What Our Clients Say'}
+                heading={'TESTIMONIALS'}
+            ></SectionTitle>
+            <div className="my-20">
+                <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+                    {
+                        reviews.map(review => <SwiperSlide
+                            key={review._id}
+                        >
+                            <div className="m-16 flex  flex-col items-center">
+                                <Rating
+                                    style={{ maxWidth: 180 }}
+                                    value={review.rating}
+                                    readOnly
+                                />
+                                <img className="h-[50px] mt-4 mb-2" src={icon} alt="" />
+                                <p>{review.details}</p>
+                                <h3 className="text-2xl text-orange-400">{review.name}</h3>
+
+                            </div>
+                        </SwiperSlide>)
+                    }
+                </Swiper>
+
+            </div>
+        </div>
+    );
+};
+
+export default Testimonials;
